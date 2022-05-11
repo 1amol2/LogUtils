cba60d4b122bcd82dfd083738bc064a11946c2c8
1amol2 <amolchandra2006@gmail.com>
2022-05-11 12:41:00 +0530

    Removed log code

--- app/src/main/java/com/amol/utils/LogUtils/MainActivity.java
+++ app/src/main/java/com/amol/utils/LogUtils/MainActivity.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import android.content.Context;
 import android.widget.Toast;
 import com.amol.utils.LogUtils.LogUtils;
-import com.itsaky.androidide.logsender.LogSender;
 
 public class MainActivity extends AppCompatActivity {
 	
@@ -14,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-		LogSender.startLogging(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		String str = "";
